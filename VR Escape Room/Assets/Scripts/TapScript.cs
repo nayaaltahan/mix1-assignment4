5e84c3d5b46a9e7520955ec64cca36d50958bac0
Naya Al Tahan <naya.altahan00@gmail.com>
2020-11-12 09:41:03 +0100

    se

---
+++ VR Escape Room/Assets/Scripts/TapScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapScript : MonoBehaviour
+{
+    public GameObject tap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tap = GameObject.Find("Tap");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TurnOnTheTap()
+    {
+        tap.SetActive(true);
+    }
+    
+    
+}
