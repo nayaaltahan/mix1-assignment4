174fd1c1e61185a17139f9a6829a652cbea8a120
unknown <naya.altahan00@gmail.com>
2020-11-12 16:32:25 +0100

    second day

---
+++ VR Escape Room/Assets/Scripts/BucketScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tap;
+    public GameObject water;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Tank")
+        {
+            //GetComponent<Rigidbody>().isKinematic(false);
+            water.SetActive(true);
+        }
+    }
+}
