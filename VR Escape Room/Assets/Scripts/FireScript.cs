786e4151f1bab8c4c6db537175b2314798f36d2a
unknown <naya.altahan00@gmail.com>
2020-11-15 16:57:44 +0100

    Sunday

---
+++ VR Escape Room/Assets/Scripts/FireScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour
+{
+    public GameObject key; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Bucket_1_1" && GameObject.Find("BucketWater").activeInHierarchy )
+        {
+            gameObject.SetActive(false);
+            Debug.Log("22222222");
+            key.SetActive(true);
+            key.GetComponent<OVRGrabbable>().enabled = true;
+        }
+    }
+}
