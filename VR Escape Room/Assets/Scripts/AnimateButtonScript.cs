9e15496ab73b6f72c64b0d6cd1071fa8d8030943
unknown <naya.altahan00@gmail.com>
2020-11-15 18:13:17 +0100

    Sunday

---
+++ VR Escape Room/Assets/Scripts/AnimateButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateButtonScript : MonoBehaviour
+{
+    public Animator animation;
+
+    public string animationName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+        animation.enabled = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "LeftHandAnchor" || other.name == "RightHandAnchor")
+        {
+            if (animation.enabled == false)
+            {
+                animation.enabled = true;
+            }
+            animation.Play(animationName);
+        }
+    }
+}
