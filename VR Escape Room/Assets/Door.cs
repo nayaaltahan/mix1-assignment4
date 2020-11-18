b794bc1cf1d78517609b81a1862f5786d9a96aaa
unknown <naya.altahan00@gmail.com>
2020-11-18 12:59:58 +0100

    it workssss

---
+++ VR Escape Room/Assets/Door.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "key_gold")
+        {
+            Debug.Log("34567890");
+            door.Rotate(Vector3.up,90);
+        }
+    }
+}
